dfc61b94cd8ce04c784fb6a7d1829fdde2d0992f
grldbelen <124864198+grldbelen@users.noreply.github.com>
2023-05-25 12:54:34 +0000

    sample variable
    introduction of variable in python

---
+++ main.py
@@ -0,0 +1,12 @@
+wallet = 4
+print(wallet)
+
+wallet = 1
+print(wallet)
+#variable holds a piece on information and can change over time
+
+#challenge
+#Make a variable called day and set it equal to the date of the month
+
+Day = 21 #example; today is the 21st day of the month
+
